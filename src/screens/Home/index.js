60438dde549874aac5c2c397a9865dcb6ada2361
SathishSaminathan <Sathishcse1975@gmail.com>
2019-05-22 16:07:01 +0530

    initial_commit

---
+++ src/screens/Home/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class Home extends Component {
+    render () {
+        return (
+            <div>
+                Hai
+            </div>
+        )
+    }
+}
+
+export default Home
